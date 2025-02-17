f4a70097dcb9ce4bbdb86ef06a1f45a965aaeaae
Maxwell Nyarko <nyarkomaxell03@gmail.com>
2025-02-17 09:44:33 -0700

    Registered the app in the Admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-# from bookshelf.models import Book
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+# admin.site.register(Book)
+admin.ModelAdmin(Book)
